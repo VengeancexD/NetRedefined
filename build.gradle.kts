73a85044f8a055923423e4e79d72f59e0b310620
VengeancexD <88082586+VengeancexD@users.noreply.github.com>
2025-06-04 13:50:45 +0530

    Optimized build.gradle.kts
    Optimized Dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,45 @@
 plugins {
     kotlin("jvm") version "2.2.0-RC"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
-group = "net.netrefined"
-version = "1.0"
-description = "NetRedefined - Ultimate Networking Optimizer for Minecraft"
+group = "net.refined"
+version = "1.1"
+description = "NetRedefined - High Performance Minecraft Networking Optimization Plugin"
 
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21)
+}
 
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.dmulloy2.net/repository/public/")
-    maven("https://jitpack.io") // Optional: For other advanced libs
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.3.0")
-    compileOnly("io.netty:netty-all:4.2.1.Final")
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
+
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 }
- kotlin {
-            jvmToolchain(21)
-        }
 
 tasks {
-    processResources {
-        val props = mapOf("version" to project.version)
-        inputs.properties(props)
-        filteringCharset = "UTF-8"
-        filesMatching("plugin.yml") {
-            expand(props)
-        }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "21"
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release.set(21)
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+        relocate("com.comphenix.protocol", "net.redefined.libs.protocol") // optional, if shading
+        minimize()
     }
 
     build {
         dependsOn(shadowJar)
-    }
-
-    runServer {
-        minecraftVersion("1.21")
     }
 }
